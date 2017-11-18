14db3ee93d3f2f8a042d4c3334dc88503bf93c08
chaofeng.chen <chaofeng.chen@gdit.com>
2017-11-18 11:43:09 -0500

    add subnetid for terraform

--- aws.tf
+++ aws.tf
@@ -24,7 +24,14 @@ variable "instance_type" {
   }
 }
 
-
+variable "subnet" {
+  description = "aws subnet id"
+  type = "map"
+  default = {
+    "dev" = "subnet-cae60f97"
+    "prod"  = "subnet-ad4ac3c9"
+  }
+}
 
 # configure backend state using S3 for team sharing on build state
 data "terraform_remote_state" "aws_infra" {
@@ -45,6 +52,7 @@ resource "aws_instance" "docker_swarm" {
   count = "3"
   ami = "${var.aws_ami}"
   instance_type = "${lookup(var.instance_type,var.env)}"
+  subnet_id = "${lookup(var.subnet, var.env)}"
 
   tags {
     Name = "docker_swarm"
